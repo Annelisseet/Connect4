badd730359d5665f46031df162403e22fab8acb0
Annelisse Emeterio <annelisseet8@gmail.com>
2018-03-02 15:12:42 -0400

    Modificacion de diseno inicial

--- app/src/main/java/com/example/annelisse/connect4/MainActivity.java
+++ app/src/main/java/com/example/annelisse/connect4/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.annelisse.connect4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{ //implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent7 = new Intent(this, Grid.class);
+        startActivity(intent7);
+
+       // findViewById(R.id.GridView).setOnClickListener(this);
     }
+
+
 }
