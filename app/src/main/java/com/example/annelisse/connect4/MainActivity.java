71e6f5d288f6db02575a6a3d63d7774144c7e289
Annelisse Emeterio <annelisseet8@gmail.com>
2018-03-09 14:25:31 -0400

    Agregando botones de conteo de juego

--- app/src/main/java/com/example/annelisse/connect4/MainActivity.java
+++ app/src/main/java/com/example/annelisse/connect4/MainActivity.java
@@ -1,23 +1,48 @@
 package com.example.annelisse.connect4;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.content.Intent;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity { //implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.iniciarJuego).setOnClickListener((v) ->
-                startActivity(new Intent(this, Grid.class))
+        findViewById(R.id.botonReiniciar).setOnClickListener((v) ->
+                startActivity(new Intent(this, MainActivity.class))
         );
+
+        GridView gridView = findViewById(R.id.myGridViewComponent);
+        gridView.setOnItemClickListener(this);
+
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < 42; i++) {
+            stringList.add(" ");
+        }
+        adapter = new ArrayAdapter<>(this, R.layout.connect_text_view, stringList);
+        gridView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView btn = (TextView) adapter.getView(position, view, parent);
+        int background = position % 2 == 1 ? R.drawable.round_button_red : R.drawable.round_button_yellow;
+        btn.setBackgroundResource(background);
 
     }
 }
-
