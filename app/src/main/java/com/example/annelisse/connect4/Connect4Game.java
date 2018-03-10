79450fc7dcfea175084998147a59a5543e008cd6
ariel <arielporfirio@gmail.com>
2018-03-10 03:45:14 -0400

    Verifica si hay ganador desde los diagonales

--- app/src/main/java/com/example/annelisse/connect4/Connect4Game.java
+++ app/src/main/java/com/example/annelisse/connect4/Connect4Game.java
@@ -4,14 +4,14 @@ class Connect4Game {
 
     private static final String PLAYER_1 = "player1";
     private static final String PLAYER_2 = "player2";
-    private final int fil = 6;
-    private final int col = 7;
+    private final int totalFilas = 6;
+    private final int totalCol = 7;
     private String currentPlayer;
     private String[] posiciones; //
 
     Connect4Game() {
         currentPlayer = PLAYER_1;
-        posiciones = new String[fil * col];
+        posiciones = new String[totalFilas * totalCol];
     }
 
     boolean currentPlayerEsPlayer1() {
@@ -45,15 +45,17 @@ class Connect4Game {
         return siHaGanadoHorizontal(PLAYER_1)
                 || siHaGanadoHorizontal(PLAYER_2)
                 || siHaGanadoVertical(PLAYER_1)
-                || siHaGanadoVertical(PLAYER_2);
+                || siHaGanadoVertical(PLAYER_2)
+                || siHaGanadoDiagonal(PLAYER_1)
+                || siHaGanadoDiagonal(PLAYER_2);
     }
 
     private boolean siHaGanadoHorizontal(String player) {
         int cont = 0;
-        for (int i = 0; i < fil; i++) {
-            for (int j = 0; j < col && cont < 4; j++) {
-                int pos = (i * col) + j;
-                if (player.equals(posiciones[pos])) {
+        for (int i = 0; i < totalFilas; i++) {
+            for (int j = 0; j < totalCol && cont < 4; j++) {
+                int pos = (i * totalCol) + j;
+                if (siPosicionHaSidoJugadaPor(pos, player)) {
                     cont++;
                 } else {
                     cont = 0;
@@ -70,10 +72,10 @@ class Connect4Game {
 
     private boolean siHaGanadoVertical(String player) {
         int cont = 0;
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < fil && cont < 4; j++) {
-                int pos = (j * col ) + i;
-                if (player.equals(posiciones[pos])) {
+        for (int i = 0; i < totalCol; i++) {
+            for (int j = 0; j < totalFilas && cont < 4; j++) {
+                int pos = (j * totalCol) + i;
+                if (siPosicionHaSidoJugadaPor(pos, player)) {
                     cont++;
                 } else {
                     cont = 0;
@@ -84,6 +86,125 @@ class Connect4Game {
             } else {
                 cont = 0;
 
+            }
+        }
+        return false;
+    }
+
+    private boolean siPosicionHaSidoJugadaPor(int pos, String player) {
+        return pos < posiciones.length && player.equals(posiciones[pos]);
+    }
+
+
+    private boolean siHaGanadoDiagonal(String player) {
+        return siHaGanadoDiagonalIzquierdaDerecha(player)
+                || siHaGanadoDiagonalDerechaIzquierda(player);
+    }
+
+
+    private boolean siHaGanadoDiagonalIzquierdaDerecha(String player) {
+        return siHaGanadoDiagonalIzquierdaDerechaArriba(player)
+                || siHaGanadoDiagonalIzquierdaDerechaAbajo(player);
+    }
+
+    private boolean siHaGanadoDiagonalIzquierdaDerechaArriba(String player) {
+        int cont = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int col = 0; col < 4; col++) {
+                int pos = col + (i * totalCol);
+                for (int fil = i; fil < totalFilas && (fil + (col-i) <= totalFilas) && cont < 4; fil++) {
+                    if (siPosicionHaSidoJugadaPor(pos, player)) {
+                        cont++;
+                    } else {
+                        cont = 0;
+                    }
+                    pos += totalCol + 1;
+                }
+                if (cont == 4) {
+                    return true;
+                } else {
+                    cont = 0;
+
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private boolean siHaGanadoDiagonalIzquierdaDerechaAbajo(String player) {
+        int cont = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int col = 0; col < 4; col++) {
+                int pos = 35 + col - (totalCol * i);
+                for (int fil = i; fil < totalFilas && (fil + (col-i) <= totalFilas) && cont < 4; fil++) {
+                    if (siPosicionHaSidoJugadaPor(pos, player)) {
+                        cont++;
+                    } else {
+                        cont = 0;
+                    }
+                    pos -= totalFilas;
+                }
+                if (cont == 4) {
+                    return true;
+                } else {
+                    cont = 0;
+
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private boolean siHaGanadoDiagonalDerechaIzquierda(String player) {
+        return siHaGanadoDiagonalDerechaIzquierdaArriba(player)
+                || siHaGanadoDiagonalDerechaIzquierdaAbajo(player);
+    }
+
+    private boolean siHaGanadoDiagonalDerechaIzquierdaArriba(String player) {
+        int cont = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int col = 0; col < 4; col++) {
+                int pos = (totalFilas - col) + totalCol * i;
+                for (int fil = i; fil < totalFilas && (fil + (col-i) <= totalFilas) && cont < 4; fil++) {
+                    if (siPosicionHaSidoJugadaPor(pos, player)) {
+                        cont++;
+                    } else {
+                        cont = 0;
+                    }
+                    pos += totalFilas;
+                }
+            }
+            if (cont == 4) {
+                return true;
+            } else {
+                cont = 0;
+
+            }
+
+        }
+        return false;
+    }
+
+    private boolean siHaGanadoDiagonalDerechaIzquierdaAbajo(String player) {
+        int cont = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int col = 0; col < 4; col++) {
+                int pos = ((totalFilas * totalCol - 1) - col) - (totalCol * i);
+                for (int fil = i; fil < totalFilas && (fil + (col-i) <= totalFilas) && cont < 4; fil++) {
+                    if (siPosicionHaSidoJugadaPor(pos, player)) {
+                        cont++;
+                    } else {
+                        cont = 0;
+                    }
+                    pos -= totalCol + 1;
+                }
+                if (cont == 4) {
+                    return true;
+                } else {
+                    cont = 0;
+                }
             }
         }
         return false;
